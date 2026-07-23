0fdfa6266605968ee7f85af1fc0ed5e6633464bf
raghavendra75111175-beep <raghavendra.75111175@gmail.com>
2026-07-23 21:13:31 +0530

    Connect frontend to Railway backend

--- client/src/api/studentApi.js
+++ client/src/api/studentApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/students",
+  baseURL: "https://believable-peace-production-2fc3.up.railway.app/api",
 });
 
 export const getStudents = () => API.get("/");
